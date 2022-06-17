3f2bf97e7ae4a3a92f7d9144145d87b547ae4ad5
Jan Kalina <jan.kalina@fantom.foundation>
2022-06-17 13:15:30 +0200

    Base of the benchmark

---
+++ internal/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"github.com/cockroachdb/pebble"
+	"time"
+)
+
+func main() {
+	db, err := pebble.Open("/media/sda2/opera/pebble-benchmark", &pebble.Options{})
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	printStats(db)
+	for i := uint64(0); i < 1_000_000; i++ {
+		prefix := intToBytes(i)
+		generateData(db, prefix)
+		printStats(db)
+	}
+}
+
+func printStats(db *pebble.DB) {
+	fmt.Printf("%s:\n%s\n", time.Now(), db.Metrics().String())
+}
+
+func generateData(db *pebble.DB, prefix []byte) {
+	for i := uint64(0); i < 1_000_000; i++ {
+		key := append(prefix, intToBytes(i)...)
+		err := db.Set(key, []byte{0x12, 0x34, 0x56, 0x78, 0x90}, nil)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func intToBytes(i uint64) []byte {
+	var bytes [8]byte
+	binary.LittleEndian.PutUint64(bytes[:], i)
+	return bytes[:]
+}
