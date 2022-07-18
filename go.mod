b330c224627aa8fccf4dc618398cf2b9a72c897c
Jan Kalina <jan.kalina@fantom.foundation>
2022-07-18 22:28:59 +0200

    Prometheus metrics

--- go.mod
+++ go.mod
@@ -2,25 +2,32 @@ module github.com/hkalina/pebble-benchmark
 
 go 1.17
 
-require github.com/cockroachdb/pebble v0.0.0-20220616214320-059c072fd94a
+require (
+	github.com/cockroachdb/pebble v0.0.0-20220616214320-059c072fd94a
+	github.com/prometheus/client_golang v1.12.2
+)
 
 require (
 	github.com/DataDog/zstd v1.4.5 // indirect
 	github.com/HdrHistogram/hdrhistogram-go v1.1.2 // indirect
-	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/cockroachdb/errors v1.8.1 // indirect
 	github.com/cockroachdb/logtags v0.0.0-20190617123548-eb05cc24525f // indirect
 	github.com/cockroachdb/redact v1.0.8 // indirect
 	github.com/cockroachdb/sentry-go v0.6.1-cockroachdb.2 // indirect
 	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.3 // indirect
-	github.com/google/go-cmp v0.5.4 // indirect
 	github.com/klauspost/compress v1.11.7 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.32.1 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
 	golang.org/x/exp v0.0.0-20200513190911-00229845015e // indirect
-	golang.org/x/sys v0.0.0-20210909193231-528a39cd75f3 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,44 +4,79 @@ import (
 	"encoding/binary"
 	"fmt"
 	"github.com/cockroachdb/pebble"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"math/rand"
+	"net/http"
 	"os"
+	"sync"
 	"time"
 )
 
+var insertedAmountCounter = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "pebble_benchmark_inserted_amount",
+	Help: "Amount of inserted items",
+})
+var insertCounter = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "pebble_benchmark_insert",
+	Help: "Time consumed by inserting",
+})
+var compactCounter = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "pebble_benchmark_compact",
+	Help: "Time consumed by compact",
+})
+
+var waitGroup sync.WaitGroup
+
 func main() {
-	useCompact := false
+	useCompact := os.Args[2] == "compact"
+
 	db, err := pebble.Open(os.Args[1], &pebble.Options{})
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	for i := uint64(0); i < 1_000_000; i++ {
+	waitGroup.Add(1)
+	go runPrometheusServer()
+
+	for i := 0; i < 16; i++ {
+		waitGroup.Add(1)
+		go runInserts(db, useCompact && i == 0) // compact only in one worker thread
+	}
+	waitGroup.Wait()
+}
+
+func runInserts(db *pebble.DB, useCompact bool) {
+	defer waitGroup.Done()
+
+	for i := uint64(0); true; i++ {
 		prefix := intToBytes(i)
 
 		// insert 1_000_000 records
-		insertStart := time.Now()
-		for i := uint64(0); i < 1_000_000; i++ {
-			key := append(prefix, intToBytes(i)...)
-			err := db.Set(key, []byte{0x12, 0x34, 0x56, 0x78, 0x90}, nil)
+		for ii := uint64(0); ii < 100_000; ii++ {
+			key := append(prefix, intToBytes(ii)...)
+			value := randomBytes()
+			insertStart := time.Now()
+			err := db.Set(key, value, nil)
 			if err != nil {
 				panic(err)
 			}
+			insertCounter.Add(float64(time.Since(insertStart).Nanoseconds()))
+			insertedAmountCounter.Inc()
 		}
-		insertDur := time.Since(insertStart)
-		metrcs := db.Metrics().String()
+		fmt.Printf("%s:\n%s\n", time.Now().String(), db.Metrics().String()) // print pebble metrics
 
 		// run compact - if omitted, the performance is bad
-		compactStart := time.Now()
 		if useCompact {
-			err = db.Compact([]byte{}, []byte{0xFF}, true)
+			compactStart := time.Now()
+			err := db.Compact([]byte{}, []byte{0xFF}, true)
 			if err != nil {
 				panic(err)
 			}
+			compactCounter.Add(float64(time.Since(compactStart).Nanoseconds()))
 		}
-		compactDur := time.Since(compactStart)
-
-		fmt.Printf("%s:\niteration: %d insert: %d compact: %d\n%s\n", time.Now(), i, insertDur.Milliseconds(), compactDur.Milliseconds(), metrcs)
 	}
 }
 
@@ -49,4 +84,27 @@ func intToBytes(i uint64) []byte {
 	var bytes [8]byte
 	binary.LittleEndian.PutUint64(bytes[:], i)
 	return bytes[:]
+}
+
+func randomBytes() []byte {
+	bytes := make([]byte, 40)
+	if _, err := rand.Read(bytes); err != nil {
+		panic(err)
+	}
+	return bytes
+}
+
+func runPrometheusServer() {
+	defer waitGroup.Done()
+	srv := &http.Server{
+		Addr:              ":4321",
+		ReadTimeout:       time.Second * 2,
+		WriteTimeout:      time.Second * 15,
+		IdleTimeout:       time.Second * 2,
+		ReadHeaderTimeout: time.Second * 2,
+		Handler:           promhttp.Handler(),
+	}
+	if err := srv.ListenAndServe(); err != nil {
+		fmt.Printf("prometheus failed: %s", err)
+	}
 }
